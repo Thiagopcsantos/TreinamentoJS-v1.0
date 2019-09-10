38743c60e8596c7cb6318b91c81060bffad8c6e1
Thiago Cavalcante <thiagopcsantos@gmail.com>
2019-09-10 19:38:52 -0300

    first commit

---
+++ javaScript.js
@@ -0,0 +1,21 @@
+document.getElementById('nome').value = 'Oi';
+
+
+// tipos de variaveis
+
+let texto = "curso de JavaScript";
+let  numeroInt = -7 ;
+let numeroFloat = 12312.00;
+
+let bolean = true / false;
+//  Representa o estado verdadeiro ou falso.
+
+
+document.write(texto);
+
+console.log(numeroInt);
+
+
+
+
+
